058b6e4e4f3bd4ec5e7f2b02184b2aa8b2e335a0
Tom Pullen <tpullen21@gmail.com>
2016-01-28 16:25:36 +0000

    Added class template and functionality

---
+++ client/templates/class/class.js
@@ -0,0 +1,44 @@
+Template.class.helpers({
+	students: function(){
+		return Session.get("students");
+	},
+    moduleName: function(){
+        return Session.get("moduleName");
+    },
+    moduleCode: function(){
+        return Session.get("moduleCode");
+    },
+    occurrenceStart: function(){
+        var date = moment(Session.get("occurrenceStartDatetime"), "YYYY-MM-DD HH:mm:ss");
+        return moment(date).format("ddd do MMM YY HH:mm");
+    },
+    occurrenceFinish: function(){
+        var date = moment(Session.get("occurrenceFinishDatetime"), "YYYY-MM-DD HH:mm:ss");
+        return moment(date).format("HH:mm");
+    },
+    rowClass: function(bool) {
+        return bool == 0 ? "danger" : "";
+    },
+    sessionOccurrenceIDMatchesParameterOccurrenceID: function() {
+        return Session.get("occurrenceIDFromParameter") === Session.get("occurrenceIDFromSession");
+    }
+});
+
+Template.class.onCreated(function() {
+
+    Session.set("occurrenceIDFromParameter", this.data.occurrenceID);
+
+    Meteor.call('getClassAttendance', this.data.occurrenceID, function(err, jsonResponse) {
+        if(err) {
+            console.log("error occured on receiving data on server. ", err );
+        } else {
+            console.log("JSON Response: ", jsonResponse);
+            Session.set("moduleName", jsonResponse.moduleName);
+            Session.set("moduleCode", jsonResponse.moduleCode);
+            Session.set("occurrenceStartDatetime", jsonResponse.occurrenceStartDatetime);
+            Session.set("occurrenceFinishDatetime", jsonResponse.occurrenceFinishDatetime);
+            Session.set("occurrenceIDFromSession", jsonResponse.occurrenceID);
+            Session.set("students", jsonResponse.students);
+        }
+    });
+});
--- server/common.js
+++ server/common.js
@@ -25,6 +25,19 @@ Meteor.methods({
             throw new Meteor.Error(result.statusCode, errorJson.error);
         }
     },
+    getClassAttendance: function(occurrenceID) {
+        var url = "http://itsuite.it.brighton.ac.uk/torp10/attend/getClassAttendance.php?occurrenceID=" + occurrenceID;
+        // Synchronous GET Request
+        var result = HTTP.get(url, {timeout:30000})
+        if(result.statusCode==200) {
+            var jsonResponse = JSON.parse(result.content);
+            return jsonResponse;
+        } else {
+            console.log("Response issue: ", result.statusCode);
+            var errorJson = JSON.parse(result.content);
+            throw new Meteor.Error(result.statusCode, errorJson.error);
+        }
+    },
     getMyModulesAttendance: function() {
         var url = "http://itsuite.it.brighton.ac.uk/torp10/attend/getMyModulesAttendance.php";
         // Synchronous GET Request
