0b09960619e345356d211a3016b443e7b316e081
Tom Pullen <tpullen21@gmail.com>
2016-01-29 11:04:39 +0000

    Added ability to close logged-in panel when viewing on smaller devices. Also added logout button in the navbar.

--- client/templates/layout/layout.js
+++ client/templates/layout/layout.js
@@ -8,3 +8,16 @@ Template.registerHelper('isStaff', function() {
 		}
 	}
 });
+
+Template.layout.events({
+	"click .logout-user": function (event) {
+        Meteor.logout(function (err) {
+            if (err) {
+                FlashMessages.sendError(err.reason);
+            } else {
+                FlashMessages.sendSuccess('You are now logged out.');
+                Router.go('/');
+            }
+        });
+    }
+});
