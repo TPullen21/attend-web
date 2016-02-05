3861733f6b6499b24e7babe55b9983e268e77a4e
Tom Pullen <tpullen21@gmail.com>
2016-02-05 20:02:08 +0000

    Added comments to student_module_detail.js

--- client/templates/student_module_detail/student_module_detail.js
+++ client/templates/student_module_detail/student_module_detail.js
@@ -1,30 +1,40 @@
+// Helper methods for the student_module_detail HTML template
 Template.student_module_detail.helpers({
+    // This student's name
 	studentName: function(){
 		return Session.get("studentName");
 	},
+    // This module's name
     moduleName: function(){
         return Session.get("moduleName");
     },
-	occurencesGroupedCollection: function(){
-		return Session.get("occurencesGroupedCollection");
+    // The grouped collection of class attendance information for this student and module
+	occurrencesGroupedCollection: function(){
+		return Session.get("occurrencesGroupedCollection");
 	},
+    // Used to get the class for the table row. If the student hasn't attended this class, it will be marked red with the class 'danger'
     rowClass: function(bool) {
         return bool == 0 ? "danger" : "";
     },
+    // Do the route IDs match the cached IDs?
     routeIDsMatchSessionIDs: function() {
         return Session.get("studentNumberFromParameter") === Session.get("studentNumberFromSession")
             && Session.get("moduleIDFromParameter") === Session.get("moduleIDFromSession");
     },
+    // Has any data been returned from the database?
     noDataOrAccess: function() {
         return typeof(Session.get("noDataOrAccess")) === 'undefined' ? true : Session.get("noDataOrAccess");
     }
 });
 
+// Called when the template is first created
 Template.student_module_detail.onCreated(function() {
 
+    // Set the session variables with the route student IDs
     Session.set("studentNumberFromParameter", this.data.studentNumber);
     Session.set("moduleIDFromParameter", this.data.moduleID);
 
+    // Call the server method which returns the class attendance information for this module and student
     Meteor.call('getStudentsAttendanceForModule', this.data.studentNumber, this.data.moduleID, Meteor.user()._id, function(err, jsonResponse) {
         if(err) {
             console.log("error occured on receiving data on server. ", err );
@@ -35,16 +45,19 @@ Template.student_module_detail.onCreated(function() {
             Session.set("studentNumberFromSession", Session.get("studentNumberFromParameter"));
             Session.set("moduleIDFromSession", Session.get("moduleIDFromParameter"));
 
-            if (jsonResponse.occurences.length > 0) {
-                var occurencesGrouped = _.groupBy(jsonResponse.occurences, 'month_and_year');
+            // If the occurrences field of the JSON response contains an array that has a size greater than 0, there's data
+            if (jsonResponse.occurrences.length > 0) {
+                // Group the class occurrences by the month and year field
+                var occurrencesGrouped = _.groupBy(jsonResponse.occurrences, 'month_and_year');
 
-                var occurencesGroupedCollection = _.map(Object.keys(occurencesGrouped), function(key) {
-                    var classes = _.map(Object.keys(occurencesGrouped[key]), function(classesKey) {
-                        return occurencesGrouped[key][classesKey];
+                // Map the grouped object into an iterable collection
+                var occurrencesGroupedCollection = _.map(Object.keys(occurrencesGrouped), function(key) {
+                    var classes = _.map(Object.keys(occurrencesGrouped[key]), function(classesKey) {
+                        return occurrencesGrouped[key][classesKey];
                     });
                     return {key: key, classes: classes};
                 });
-                Session.set("occurencesGroupedCollection", occurencesGroupedCollection);
+                Session.set("occurrencesGroupedCollection", occurrencesGroupedCollection);
                 Session.set("noDataOrAccess", false);
             } else {
                 Session.set("noDataOrAccess", true);
