d3e9f4b27270883c383ff3e8bf050f6ea504840e
Tom Pullen <tpullen21@gmail.com>
2016-02-05 13:23:51 +0000

    Added comments to class.js file

--- client/templates/class/class.js
+++ client/templates/class/class.js
@@ -1,36 +1,48 @@
+// Helper methods for the class HTML template
 Template.class.helpers({
+    // The collection of student attendance information for this class
 	students: function(){
 		return Session.get("students");
 	},
+    // The module name for this class
     moduleName: function(){
         return Session.get("moduleName");
     },
+    // The module code for this class
     moduleCode: function(){
         return Session.get("moduleCode");
     },
+    // The formatted start date and time for the occurrence of this class
     occurrenceStart: function(){
         var date = moment(Session.get("occurrenceStartDatetime"), "YYYY-MM-DD HH:mm:ss");
         return moment(date).format("ddd do MMM YY HH:mm");
     },
+    // The formatted finish time for the occurrence of this class
     occurrenceFinish: function(){
         var date = moment(Session.get("occurrenceFinishDatetime"), "YYYY-MM-DD HH:mm:ss");
         return moment(date).format("HH:mm");
     },
-    rowClass: function(bool) {
-        return bool == 0 ? "danger" : "";
+    // Used to get the class for the table row. If the student hasn't attended this class, it will be marked red with the class 'danger'
+    rowClass: function(attended) {
+        return attended == 0 ? "danger" : "";
     },
+    // Does the route occurence ID match the cached ID?
     sessionOccurrenceIDMatchesParameterOccurrenceID: function() {
         return Session.get("occurrenceIDFromParameter") === Session.get("occurrenceIDFromSession");
     },
+    // Has any data been returned from the database?
     noDataOrAccess: function() {
         return typeof(Session.get("noDataOrAccess")) === 'undefined' ? true : Session.get("noDataOrAccess");
     }
 });
 
+// Called when the template is first created
 Template.class.onCreated(function() {
 
+    // Set a session variable with the route occurence ID
     Session.set("occurrenceIDFromParameter", this.data.occurrenceID);
 
+    // Call the server method which returns the attendance information for this class from the database
     Meteor.call('getClassAttendance', this.data.occurrenceID, Meteor.userId(), function(err, jsonResponse) {
         if(err) {
             console.log("error occured on receiving data on server. ", err );
@@ -42,6 +54,7 @@ Template.class.onCreated(function() {
             Session.set("occurrenceFinishDatetime", jsonResponse.occurrenceFinishDatetime);
             Session.set("occurrenceIDFromSession", Session.get("occurrenceIDFromParameter"));
 
+            // If the students array field of the JSON response is larger than 0, there's data
             if (jsonResponse.students.length > 0) {
                 Session.set("students", jsonResponse.students);
                 Session.set("noDataOrAccess", false);
